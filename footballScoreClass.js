cf7c71d7e9dfdb6e45c0847605b87e8889d23963
Stefan Gabriel Ivan <stefanivan41@gmail.com>
2025-11-12 17:23:59 +0200

    Added class methods for goal scoring and getting the score

--- footballScoreClass.js
+++ footballScoreClass.js
@@ -1,16 +1,40 @@
 class Match {
   constructor(team1, team2, additionalInformations) {
     this.team1 = {
+      name: team1.name,
       players: team1.players,
       coach: team1.coach,
     };
 
     this.team2 = {
+      name: team2.name,
       players: team2.players,
       coach: team2.coach,
     };
 
     this.additionalInformations = additionalInformations;
+
+    this.team1Goals = [];
+    this.team2Goals = [];
+
+    this.team1Score = 0;
+    this.team2Score = 0;
+  }
+
+  addGoal({ playerName, minute }) {
+    if (this.team1.players.includes(playerName)) {
+      this.team1Goals.push({ playerName, minute });
+      this.team1Score++;
+    } else if (this.team2.players.includes(playerName)) {
+      this.team2Goals.push({ playerName, minute });
+      this.team2Score++;
+    } else {
+      console.error("Player not found: ", playerName);
+    }
+  }
+
+  getScore() {
+    return `${this.team1.name} ${this.team1Score} - ${this.team2Score} ${this.team2.name}`;
   }
 }
 
@@ -29,6 +53,7 @@ const team1 = {
     "Martinelli",
     "Jesus",
   ],
+  coach: "Arteta",
 };
 
 const team2 = {
@@ -46,4 +71,9 @@ const team2 = {
     "Rashford",
     "Højlund",
   ],
+  coach: "Amorim",
 };
+
+const match = new Match(team1, team2, additionalInformations);
+match.addGoal({ player: "Saka", minute: 23 });
+match.getScore();
